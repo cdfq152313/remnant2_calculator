1f0a626a4bb9c62ec5e05dbd7632ec760653bda6
Denny Cheng <cdfq152313@gmail.com>
2023-11-25 13:13:16 +0800

    First ui

--- lib/view/CharacterView.dart
+++ lib/view/CharacterView.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:remnant2_calculator/data/item.dart';
 import 'package:remnant2_calculator/domain/calculator_cubit.dart';
 import 'package:remnant2_calculator/domain/character_cubit.dart';
 
@@ -8,14 +9,54 @@ class CharacterView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (_) => CharacterCubit(),
-      child: BlocProvider(
-        create: (_) => CalculatorCubit(),
-        child: BlocListener<CharacterCubit, CharacterState>(
-          listener: (BuildContext context, state) {},
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (_) => CharacterCubit(),
         ),
+        BlocProvider(
+          create: (_) => CalculatorCubit(),
+        ),
+      ],
+      child: BlocListener<CharacterCubit, CharacterState>(
+        listener: (BuildContext context, state) {
+          context.read<CalculatorCubit>().update(state);
+        },
+        child: const _CharacterView(),
       ),
+    );
+  }
+}
+
+class _CharacterView extends StatelessWidget {
+  const _CharacterView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          children: [
+            const Text('主職業'),
+            BlocBuilder<CharacterCubit, CharacterState>(
+                builder: (context, state) {
+              return DropdownButton(
+                items: archetypes
+                    .map(
+                      (e) => DropdownMenuItem(
+                        value: e,
+                        child: Text(e.name),
+                      ),
+                    )
+                    .toList(),
+                value: state.primaryArchetype,
+                onChanged: (v) =>
+                    context.read<CharacterCubit>().setPrimaryArchetype(v),
+              );
+            }),
+          ],
+        ),
+      ],
     );
   }
 }

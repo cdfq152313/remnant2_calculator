1752c6c62535dbce7c73b31d5c4f0210d8116594
Denny Cheng <cdfq152313@gmail.com>
2023-11-29 19:56:23 +0800

    Format items

--- lib/data/item.dart
+++ lib/data/item.dart
@@ -17,222 +17,170 @@ final itemMap = Map.fromEntries([
 ].map((v) => MapEntry(v.name, v)));
 
 final archetypes = [
-  const Item(
+  Item(
     name: '獵人',
     effects: [
-      Effect(
-        type: EffectType.damageIncrease,
-        value: 40,
-        damageTypes: [DamageType.range],
-      ),
-      Effect(
-        type: EffectType.weakSpotDamage,
-        value: 15,
-        damageTypes: [DamageType.range],
-      ),
-      Effect(
-        type: EffectType.criticalChance,
-        value: 5,
-        damageTypes: [DamageType.range],
-      ),
+      Effect.damageIncrease(40, damageTypes: [DamageType.range]),
+      Effect.weakSpotDamage(15, damageTypes: [DamageType.range]),
+      Effect.criticalChance(5, damageTypes: [DamageType.range]),
     ],
   ),
-  const Item(
+  Item(
     name: '槍手',
     effects: [
-      Effect(
-        type: EffectType.damageIncrease,
-        value: 25,
-        damageTypes: [DamageType.range],
-      ),
-      Effect(
-        type: EffectType.criticalChance,
-        value: 5,
-        damageTypes: [DamageType.range],
-      ),
+      Effect.damageIncrease(25, damageTypes: [DamageType.range]),
+      Effect.criticalChance(5, damageTypes: [DamageType.range]),
     ],
   ),
-  const Item(
+  Item(
     name: '醫療兵',
     effects: [
-      Effect(type: EffectType.damageIncrease, value: 25),
-      Effect(type: EffectType.criticalChance, value: 5),
+      Effect.damageIncrease(25),
+      Effect.criticalChance(5),
     ],
   ),
 ];
 
 final modifiers = [
-  const Item(
+  Item(
     name: '獵人1技能',
     effects: [
-      Effect(
-        type: EffectType.damageIncrease,
-        value: 15,
-        damageTypes: [DamageType.range],
-      ),
-      Effect(
-        type: EffectType.criticalChance,
-        value: 15,
-        damageTypes: [DamageType.range],
-      ),
+      Effect.damageIncrease(15, damageTypes: [DamageType.range]),
+      Effect.criticalChance(15, damageTypes: [DamageType.range]),
     ],
   ),
 ];
 
 final longGuns = [
-  const Weapon(
+  Weapon(
     name: '日暮',
-    damage: BaseDamage(93, [DamageType.range]),
+    damage: const BaseDamage(93, [DamageType.range]),
     effects: [
-      Effect(type: EffectType.criticalChance, value: 5),
-      Effect(type: EffectType.weakSpotDamage, value: 105),
+      Effect.criticalChance(5),
+      Effect.weakSpotDamage(105),
     ],
   ),
 ];
 
 final handGuns = [
-  const Weapon(
+  Weapon(
     name: 'MP60-R',
-    damage: BaseDamage(27, [DamageType.range]),
+    damage: const BaseDamage(27, [DamageType.range]),
     effects: [
-      Effect(type: EffectType.criticalChance, value: 10),
-      Effect(type: EffectType.weakSpotDamage, value: 100),
+      Effect.criticalChance(10),
+      Effect.weakSpotDamage(100),
     ],
   ),
 ];
 
 final melees = [
-  const Weapon(
+  Weapon(
     name: '科雷爾斧',
-    damage: BaseDamage(162, [DamageType.melee]),
+    damage: const BaseDamage(162, [DamageType.melee]),
     effects: [
-      Effect(type: EffectType.criticalChance, value: 3),
-      Effect(type: EffectType.weakSpotDamage, value: 85),
+      Effect.criticalChance(3),
+      Effect.weakSpotDamage(85),
     ],
   ),
 ];
 
 final amulets = [
-  const Item(
+  Item(
     name: '腐蝕磨石',
     effects: [
-      Effect(type: EffectType.criticalChance, value: 15),
-      Effect(type: EffectType.criticalDamage, value: 30),
+      Effect.criticalChance(15),
+      Effect.criticalDamage(30),
     ],
   ),
-  const Item(
+  Item(
     name: '聖十字輝光',
     effects: [
-      Effect(type: EffectType.damageIncrease, value: 15),
+      Effect.damageIncrease(15),
     ],
   ),
 ];
 
 final rings = [
-  const Item(
+  Item(
     name: '扎尼亞的惡意',
     effects: [
-      Effect(type: EffectType.weakSpotDamage, value: 30),
+      Effect.weakSpotDamage(30),
     ],
   ),
-  const Item(
+  Item(
     name: '戰爭指環',
     effects: [
-      Effect(type: EffectType.criticalChance, value: 15),
-      Effect(type: EffectType.criticalDamage, value: 15),
+      Effect.criticalChance(15),
+      Effect.criticalDamage(15),
     ],
   ),
-  const Item(
+  Item(
     name: '機率之圈',
     effects: [
-      Effect(type: EffectType.criticalDamage, value: 30),
+      Effect.criticalDamage(30),
     ],
   ),
-  const Item(
+  Item(
     name: '破壞者之負擔',
     effects: [
-      Effect(type: EffectType.damageIncrease, value: 15),
+      Effect.damageIncrease(15),
     ],
   ),
 ];
 
 final relicFragments = [
-  const Item(
+  Item(
     name: '遠程傷害',
     effects: [
-      Effect(
-        type: EffectType.damageIncrease,
-        value: 10,
-        damageTypes: [DamageType.range],
-      ),
+      Effect.damageIncrease(10, damageTypes: [DamageType.range]),
     ],
   ),
-  const Item(
+  Item(
     name: '遠程暴擊率',
     effects: [
-      Effect(
-        type: EffectType.criticalChance,
-        value: 10,
-        damageTypes: [DamageType.range],
-      ),
+      Effect.criticalChance(10, damageTypes: [DamageType.range]),
     ],
   ),
-  const Item(
+  Item(
     name: '遠程暴擊傷害',
     effects: [
-      Effect(
-        type: EffectType.criticalDamage,
-        value: 20,
-        damageTypes: [DamageType.range],
-      ),
+      Effect.criticalDamage(20, damageTypes: [DamageType.range]),
     ],
   ),
 ];
 
 final rangeMutators = [
-  const Item(
+  Item(
     name: '動量',
     effects: [
-      Effect(type: EffectType.criticalChance, value: 30),
-      Effect(type: EffectType.criticalDamage, value: 30),
+      Effect.criticalChance(30),
+      Effect.criticalDamage(30),
     ],
   ),
-  const Item(
+  Item(
     name: '扭曲傷口',
     effects: [
-      Effect(type: EffectType.damageIncrease, value: 20),
+      Effect.damageIncrease(20),
     ],
   ),
-  const Item(
+  Item(
     name: '調諧者',
     effects: [
-      Effect(
-        type: EffectType.damageIncrease,
-        value: 20,
-        damageTypes: [DamageType.mod],
-      ),
+      Effect.damageIncrease(20, damageTypes: [DamageType.mod]),
     ],
   ),
-  const Item(
+  Item(
     name: '失效保護',
     effects: [
-      Effect(
-        type: EffectType.damageIncrease,
-        value: 20,
-        damageTypes: [DamageType.mod],
-      ),
+      Effect.damageIncrease(20, damageTypes: [DamageType.mod]),
     ],
   ),
 ];
 final meleeMutators = [
-  const Item(
+  Item(
     name: '污穢之刃',
     effects: [
-      Effect(
-        type: EffectType.damageIncrease,
-        value: 20,
-        damageTypes: [DamageType.melee],
-      ),
+      Effect.damageIncrease(20, damageTypes: [DamageType.melee]),
     ],
   ),
 ];
